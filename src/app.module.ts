cfc8a27916e558e88c2e4465e75e699b4fe5fdd2
Alex <146011365+alexxcalle@users.noreply.github.com>
2024-05-13 20:08:11 -0500

    finish

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,25 +1,21 @@
+import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { CreaturesModule } from './creatures/creatures.module';
-
-import { MyConfigModule } from './config.module';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({
-      isGlobal: true,
-    }),
     TypeOrmModule.forRoot({
       type: 'postgres',
-      url: process.env.DB_URL, // Use the environment variable here
-      autoLoadEntities: true,
+      host: process.env.POSTGRES_HOST,
+      port: parseInt(process.env.POSTGRES_PORT),
+      password: process.env.POSTGRES_PASSWORD,
+      username: process.env.POSTGRES_USER,
+      entities: [],
+      database: process.env.POSTGRES_DATABASE,
       synchronize: false,
+      logging: true,
     }),
-    CreaturesModule,
-    MyConfigModule, // Add your config module here
   ],
   controllers: [AppController],
   providers: [AppService],
